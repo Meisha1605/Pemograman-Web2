66baa21f4ea4bc3e9cc33ec0861f964ccd5cecf6
myshabtl <meishabongiteluma2005@gmail.com>
2025-03-07 14:31:27 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum/belajarphp2025-main/admin/panggil.php
@@ -0,0 +1,20 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <?php 
+                    include_once 'menu.php';
+                ?>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Hasil Tugas_2</h1>
+
+                <?php 
+                    include_once 'Tugas_2/index.php';
+                ?>
+                </div>
+</div>
+<?php 
+     include_once 'bottom.php';
+?>
